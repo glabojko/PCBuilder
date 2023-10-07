5ba253552dc65a132cd187fdac6923e7bcf948b9
glabojko <glabojko@gmail.com>
2023-10-07 11:00:30 +0200

    class CPU added

---
+++ CPU.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCBuilder
+{
+    public class CPU : ElectricalComponent
+    {
+        private static CPU? instance;
+        public CPU(string name, int powerUsage) : base(name, powerUsage)
+        {
+
+        }
+
+        public static CPU Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CPU("IntelCoreI5", -50);
+                }
+                return instance;
+            }
+        }
+    }
+}
