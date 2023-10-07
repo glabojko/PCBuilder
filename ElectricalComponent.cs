cdf94f23244fb367a88bf846df5d01e940139b37
glabojko <glabojko@gmail.com>
2023-10-07 10:48:23 +0200

    ElectricalComponent class added

---
+++ ElectricalComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCBuilder
+{
+    public class ElectricalComponent
+    {
+        string Name { get; set; }
+        int PowerUsage { get; set; }
+
+        public ElectricalComponent(string name, int powerUsage)
+        {
+            Name = name;
+            PowerUsage = powerUsage;
+        }
+    }
+}
