fb1a652501f4e31158195fa33cac43d7f339a72d
glabojko <glabojko@gmail.com>
2023-10-07 10:44:22 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PCBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witamy w aplikacji do budowy komputera osobistego!");
+        }
+    }
+}
